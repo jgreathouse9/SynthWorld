a567a557d503e87e3677fe7c8ba15cd40fd992a8
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-02-19 13:35:02 -0500

    Update WeedMap.py

--- Python/Chapter2/WeedMap.py
+++ Python/Chapter2/WeedMap.py
@@ -1,10 +1,9 @@
 from utils.dataget import get_weed_policies
 #import utils.datadisp as datadisp
 import os
-print(f"Current working directory: {os.getcwd()}")
 
 # Path to the Excel file with cannabis policies
-file_path = r"../../PolicyData/Cannabis/CannabisLaws.xlsx"
+file_path = os.path.join(os.getcwd(), 'PolicyData', 'Cannabis', 'CannabisLaws.xlsx')
 
 # Get states where cannabis is legal
 cannabis_states = get_weed_policies(file_path)
