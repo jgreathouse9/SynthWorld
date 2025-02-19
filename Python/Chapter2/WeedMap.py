fdccd983d33405d412d5cc548cb1b061906ccf5a
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-02-19 12:59:20 -0500

    Update WeedMap.py

--- Python/Chapter2/WeedMap.py
+++ Python/Chapter2/WeedMap.py
@@ -1,11 +1,11 @@
-import utils.dataget as dataget
-import utils.datadisp as datadisp
+from utils.dataget import get_weed_policies
+#import utils.datadisp as datadisp
 
 # Path to the Excel file with cannabis policies
-file_path = r"C:\The Shop\CannabisLaws.xlsx"
+file_path = r"PolicyData\CannabisLaws.xlsx"
 
 # Get states where cannabis is legal
-cannabis_states = dataget.get_weed_policies(file_path)
-
+cannabis_states = get_weed_policies(file_path)
+print(cannabis_states)
 # Plot the map
-datadisp.plot_policy_map(cannabis_states, "States with Legal Cannabis", "#008000")
+#datadisp.plot_policy_map(cannabis_states, "States with Legal Cannabis", "#008000")
