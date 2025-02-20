844d3f0c6354a981b659adbbe4f68afa7f7b3683
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-02-20 08:48:32 -0500

    Update WeedMap.py

--- Python/Chapter2/WeedMap.py
+++ Python/Chapter2/WeedMap.py
@@ -7,6 +7,8 @@ file_path = os.path.join(os.getcwd(), 'PolicyData', 'Cannabis', 'CannabisLaws.xl
 
 statelist = get_weed_policies(file_path)
 
+remove_states = {"CA", "CO", "OR", "WA", "MA", "NV", "IL"}
+
 purecontrols = list(set(statelist) - remove_states)
 
 plot_policy_map(purecontrols, title="Legal Cannabis States", color="green", save_path="Figures/Chapter2/weed_map.png")
