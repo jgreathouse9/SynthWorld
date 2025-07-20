1f7b426adc18ea8c8965e8ff5dbff6d48a856e61
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-07-20 10:51:19 -0400

    Create getdata.py

---
+++ Paper1/Paper/Code/Python/getdata.py
@@ -0,0 +1,11 @@
+import os
+from helpers import load_hawaii_data
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))  # up two levels
+data_path = os.path.join(base_dir, "Data", "HawaiiData.xlsx")
+
+load_hawaii_data(
+    save_excel=True,
+    filename=data_path,
+    compute_growth=True
+)
