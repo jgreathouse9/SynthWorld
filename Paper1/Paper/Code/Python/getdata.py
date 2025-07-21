1ad5f1eb62711547d64093397ce7a4c91b5176b2
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-07-21 11:01:47 -0400

    Update getdata.py

--- Paper1/Paper/Code/Python/getdata.py
+++ Paper1/Paper/Code/Python/getdata.py
@@ -4,8 +4,4 @@ from helpers import load_hawaii_data
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))  # up two levels
 data_path = os.path.join(base_dir, "Data", "HawaiiData.xlsx")
 
-load_hawaii_data(
-    save_excel=True,
-    filename=data_path,
-    compute_growth=True
-)
+load_hawaii_data(save_excel=True,filename=data_path)
