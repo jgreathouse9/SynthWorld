13fff047f3c9123ed9e3cdcda089b5a65aff2bb8
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-07-21 15:03:28 -0400

    Update helpers.py

--- Paper1/Paper/Code/Python/helpers.py
+++ Paper1/Paper/Code/Python/helpers.py
@@ -35,7 +35,7 @@ def load_hawaii_data(compute_growth=True, save_excel=False, filename="hawaii_dat
     def load_fred():
         fred_url = (
             "https://fred.stlouisfed.org/graph/fredgraph.csv?"
-            "id=HILEIHN,HIUR,LBSSA15,SMU15000007072100001SA,HICOINC"
+            "id=HILEIHN,HIUR,LBSSA15,SMU15000007072100001SA,HIPHCI"
             "&cosd=1990-01-01&coed=2020-12-31"
             "&fq=Monthly&fam=avg&transformation=pc1&line_index=1,2,3,4,5"
         )
@@ -49,7 +49,7 @@ def load_hawaii_data(compute_growth=True, save_excel=False, filename="hawaii_dat
             "HIUR_PC1": "Unemp Rate",
             "LBSSA15_PC1": "LFP",
             "SMU15000007072100001SA_PC1": "Accomodation Emp",
-            "HICOINC_PC1": "Econ Activity Index"
+            "HIPHCI_PC1": "Econ Activity Index"
         })
         df = df.set_index("Date")
         df = df.reindex(full_index)
