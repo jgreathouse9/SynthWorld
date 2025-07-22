d4ccb667b03d573e7ef95dc8fe161d7d53246d09
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2025-07-21 23:41:03 -0400

    Update getdata.py

--- Paper1/Paper/Code/Python/getdata.py
+++ Paper1/Paper/Code/Python/getdata.py
@@ -1,7 +1,10 @@
 import os
-from helpers import load_hawaii_data
+from helpers import load_hawaii_data, get_taxdata
 
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))  # up two levels
-data_path = os.path.join(base_dir, "Data", "HawaiiData.xlsx")
+data_pathtourism = os.path.join(base_dir, "Data", "HawaiiData.xlsx")
+data_pathtax = os.path.join(base_dir, "Data", "HawaiiData.xlsx")
 
-load_hawaii_data(save_excel=True,filename=data_path)
+load_hawaii_data(save_excel=True,filename=data_pathtourism)
+
+get_taxdata(save_excel=True,filename=data_pathtax)
